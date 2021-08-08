24faf82213141c9e0d064550661fa9d267f0d802
naga-naga <kiyoharu.639721@gmail.com>
2021-08-08 15:59:59 +0900

    一人の従業員の曜日方向の探索を作成

---
+++ main.pl
@@ -0,0 +1,29 @@
+% solve(最大勤務人数(リスト), 勤務可能日(リストのリスト), 最大連勤日数(定数), シフト(リストのリスト))
+% solve(MaxWorkers, CanWork, MaxConsecutiveWork, Shift)
+solve([], [], _, Shift).
+solve([], Canwork, _, Shift).
+solve([MaxWorkersToday | MaxWorkers], [CanWorkEmployee | CanWork], MaxConsecutiveWork, [ShiftEmployee | Shift]) :-
+    true.
+
+% 従業員一人分の探索
+employee([], [], [], _, []).
+% その曜日に従業員が働けない場合
+employee([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
+    employee(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
+    CanWorkToday = 0,
+    ShiftToday = 0,
+    MaxWokersAfterToday = MaxWokersBeforeToday.
+% その曜日に従業員が働けて，まだシフトが埋まっていない場合
+employee([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
+    employee(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
+    CanWorkToday = 1,
+    MaxWokersBeforeToday > 0,
+    ShiftToday = 1,
+    MaxWokersAfterToday is MaxWokersBeforeToday - 1.
+% その曜日に従業員が働けるが，シフトが埋まっている場合
+employee([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
+    employee(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
+    CanWorkToday = 1,
+    MaxWokersBeforeToday =< 0,
+    ShiftToday = 0,
+    MaxWokersAfterToday = MaxWokersBeforeToday.
