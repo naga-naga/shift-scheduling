bc9e0bb00ad3630fc0b4641cf4d8d1e0863ca745
naga-naga <kiyoharu.639721@gmail.com>
2021-08-10 13:48:54 +0900

    述語名を変更

--- main.pl
+++ main.pl
@@ -2,27 +2,27 @@
 % solve(MaxWorkers, CanWork, MaxConsecutiveWork, Shift)
 solve(_, [], _, []).
 solve(MaxWorkers, [CanWorkEmployee | CanWork], MaxConsecutiveWork, [ShiftEmployee | Shift]) :-
-    employee(MaxWorkers, After, CanWorkEmployee, MaxConsecutiveWork, ShiftEmployee),
+    search_employee_shift(MaxWorkers, After, CanWorkEmployee, MaxConsecutiveWork, ShiftEmployee),
     solve(After, CanWork, MaxConsecutiveWork, Shift).
 
 % 従業員一人分の探索
-employee([], [], [], _, []).
+search_employee_shift([], [], [], _, []).
 % その曜日に従業員が働けない場合
-employee([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
-    employee(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
+search_employee_shift([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
+    search_employee_shift(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
     CanWorkToday = 0,
     ShiftToday = 0,
     MaxWokersAfterToday = MaxWokersBeforeToday.
 % その曜日に従業員が働けて，まだシフトが埋まっていない場合
-employee([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
-    employee(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
+search_employee_shift([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
+    search_employee_shift(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
     CanWorkToday = 1,
     MaxWokersBeforeToday > 0,
     ShiftToday = 1,
     MaxWokersAfterToday is MaxWokersBeforeToday - 1.
 % その曜日に従業員が働けるが，シフトが埋まっている場合
-employee([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
-    employee(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
+search_employee_shift([MaxWokersBeforeToday | MaxWokersBefore], [MaxWokersAfterToday | MaxWokersAfter], [CanWorkToday | CanWork], MaxConsecutiveWork, [ShiftToday | Shift]) :-
+    search_employee_shift(MaxWokersBefore, MaxWokersAfter, CanWork, MaxConsecutiveWork, Shift),
     CanWorkToday = 1,
     MaxWokersBeforeToday =< 0,
     ShiftToday = 0,
