b1b0ef1a672c2cc67a3b67998f4220d0233d47fd
naga-naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2021-08-16 00:28:14 +0900

    最大連勤日数を指定できるようになった

--- main.pl
+++ main.pl
@@ -1,9 +1,10 @@
 % solve(最大勤務人数(リスト), 勤務可能日(リストのリスト), シフト(リストのリスト))
 % solve(MaxWorkers, CanWork, Shift)
-solve(_, [], []).
-solve(MaxWorkers, [CanWorkEmployee | CanWork], [ShiftEmployee | Shift]) :-
+solve(_, [], _, []).
+solve(MaxWorkers, [CanWorkEmployee | CanWork], MaxDays, [ShiftEmployee | Shift]) :-
     search_employee_shift(MaxWorkers, After, CanWorkEmployee, ShiftEmployee),
-    solve(After, CanWork, Shift).
+    less_than_max_days(ShiftEmployee, ShiftEmployee, MaxDays, MaxDays, 7),
+    solve(After, CanWork, MaxDays, Shift).
 
 % 従業員一人分の探索
 % search_employee_shift(最大勤務人数のリスト，更新後の最大勤務人数のリスト，勤務可能日，シフト)
@@ -24,3 +25,29 @@ search_employee_shift([MaxWorkersBeforeToday | MaxWorkersBefore], [MaxWorkersBef
     % シフトが埋まっている場合は働かない
     MaxWorkersBeforeToday =< 0,
     search_employee_shift(MaxWorkersBefore, MaxWorkersAfter, CanWork, Shift).
+
+% シフトが最大連勤日数以下かどうかを調べる
+% less_than_max_days(1週間のシフト, 調べている途中のシフト, 最大連勤可能日数, 最大連勤日数を調べるための変数, ループ回数)
+less_than_max_days(_, _, _, _, 0) :- !. % 最後まで失敗しなければ成功
+less_than_max_days(_, [1 | _], _, MaxDays, Loop) :-
+    % 最大日数以上の1が並んでいる場合は失敗
+    Loop > 0,
+    MaxDays2 is MaxDays - 1,
+    MaxDays2 < 0, !,
+    fail.
+less_than_max_days(WeeklyShift, [1 | Shift], OriginalMaxDays, MaxDays, Loop) :-
+    % 最大日数以下で1を見つけた場合は次の日を見る
+    Loop > 0,
+    MaxDays2 is MaxDays - 1,
+    MaxDays2 >= 0, !,
+    less_than_max_days(WeeklyShift, Shift, OriginalMaxDays, MaxDays2, Loop).
+less_than_max_days(WeeklyShift, [0 | _], OriginalMaxDays, _, Loop) :- !,
+    % 0を見つけたらシフトの順番を入れ替えて最大日数以下かどうかを調べる
+    Loop > 0,
+    Loop2 is Loop - 1,
+    bring_head_to_last(WeeklyShift, ChangedShift),
+    less_than_max_days(ChangedShift, ChangedShift, OriginalMaxDays, OriginalMaxDays, Loop2).
+
+% リストの先頭要素を最後尾に持ってくる
+bring_head_to_last([Head | Tail], NewList) :-
+    append(Tail, [Head], NewList).
